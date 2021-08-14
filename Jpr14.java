a2b71781e20d603c92195e9632727382902f7a1f
Suyash N. Shinde <88643417+SuyashNShinde@users.noreply.github.com>
2021-08-14 13:36:23 +0530

    14/08/21

---
+++ Jpr14.java
@@ -0,0 +1,13 @@
+package com.google;
+
+public class JPR14 {
+    public static void main(String[] args) {
+        int num=17;
+        if (num%2==0){
+            System.out.println("Number is Even.");
+        }
+        else{
+            System.out.println("Number is odd.");
+        }
+    }
+}
