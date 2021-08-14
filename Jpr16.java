1cbabfa5675ff6e0469f7b7e8bc52a7e95c3c348
Suyash N. Shinde <88643417+SuyashNShinde@users.noreply.github.com>
2021-08-14 13:37:55 +0530

    14/08/21

---
+++ Jpr16.java
@@ -0,0 +1,11 @@
+package com.google;
+//code for gst calculating
+public class JPR16 {
+    public static void main(String[] args) {
+        float GST;
+        int basePrice=34900;
+        GST=basePrice*(18.0f/100);
+        System.out.println("You have to pay RS. "+GST+"GST amount on RS. 34900");
+
+    }
+}
