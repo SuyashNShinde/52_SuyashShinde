598399f99e7a202a50460c2c1b8123abe56405c2
Suyash N. Shinde <88643417+SuyashNShinde@users.noreply.github.com>
2021-08-14 13:32:04 +0530

    Assignment No.8

---
+++ Jpr8.java
@@ -0,0 +1,24 @@
+package com.google;
+/*
+Author: Vaibhavi Dixit
+code for compound ssignment operators
+ */
+public class JPR8 {
+    public static void main(String[] args) {
+        int result=1;
+        result+=2;
+        System.out.println(result);//result is 3
+        result--;
+        System.out.println(result);//result is 2
+        result*=2;
+        System.out.println(result);//result is 4
+        result/=2;
+        System.out.println(result);//result is 2
+        result+=8;
+        System.out.println(result);//result is 10
+        result%=7;
+        System.out.println(result);//result is 3
+
+
+    }
+}
