340186949105bcf6bc05f3a0e77dbc959356308f
Suyash N. Shinde <88643417+SuyashNShinde@users.noreply.github.com>
2021-08-14 13:34:55 +0530

    14/08/2021

---
+++ Jpr12.java
@@ -0,0 +1,10 @@
+package com.google;
+
+public class JPR12 {
+    public static void main(String[] args) {
+        int a=30;
+        int b=12;
+        System.out.println("Quotient of a/b is "+a/b);//quotient
+        System.out.println("Remainder of a%b is "+a%b);//remainder
+    }
+}
