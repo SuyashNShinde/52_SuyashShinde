a40685776bbd4ff710ca5fc5dae6c1dda54f43a7
Suyash N. Shinde <88643417+SuyashNShinde@users.noreply.github.com>
2021-08-14 13:34:11 +0530

    14/08/2021

---
+++ Jpr11.java
@@ -0,0 +1,12 @@
+package com.google;
+
+public class JPR11 {
+    public static void main(String[] args) {
+
+        int x='x';
+        int X='X';
+        System.out.println("ASCII of x "+ x);
+        System.out.println("ASCII of X "+ X);
+ 
+    }
+}
